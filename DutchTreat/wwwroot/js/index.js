cfd932662c3d7d54bfdbc6fd8bb1d3c8924fcb8c
jaideepjha <jaideepjha@gmail.com>
2020-03-29 17:22:25 +0530

    added jQuery event support for div Ids and classes

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,4 +1,23 @@
 ﻿console.log("Hello World!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+//var theForm = document.getElementById("theForm");
+//theForm.hidden = true;
+
+var theForm = $("#theForm");
+theForm.hide();
+
+
+//var button = document.getElementById("theButton");
+//button.addEventListener("click", function () {
+//    console.log("Buying item.");
+//});
+
+var button = $("#theButton");
+button.on("click", function () {
+    console.log("Buying item.");
+});
+
+var prodInfo = $(".product-props li");
+prodInfo.on("click", function () {
+    console.log("you clicked: " + $(this).text());
+});
