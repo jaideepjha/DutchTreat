c6f2c9f67521509cd067b108d49686d60461812f
jaideepjha <jaideepjha@gmail.com>
2020-03-29 17:48:16 +0530

    wrapped index.js in jquery's document.ready

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,23 +1,26 @@
-﻿console.log("Hello World!");
+﻿$(document).ready(function () {
 
-//var theForm = document.getElementById("theForm");
-//theForm.hidden = true;
+    console.log("Hello World!");
 
-var theForm = $("#theForm");
-theForm.hide();
+    //var theForm = document.getElementById("theForm");
+    //theForm.hidden = true;
+
+    var theForm = $("#theForm");
+    theForm.hide();
 
 
-//var button = document.getElementById("theButton");
-//button.addEventListener("click", function () {
-//    console.log("Buying item.");
-//});
+    //var button = document.getElementById("theButton");
+    //button.addEventListener("click", function () {
+    //    console.log("Buying item.");
+    //});
 
-var button = $("#theButton");
-button.on("click", function () {
-    console.log("Buying item.");
-});
+    var button = $("#theButton");
+    button.on("click", function () {
+        console.log("Buying item.");
+    });
 
-var prodInfo = $(".product-props li");
-prodInfo.on("click", function () {
-    console.log("you clicked: " + $(this).text());
+    var prodInfo = $(".product-props li");
+    prodInfo.on("click", function () {
+        console.log("you clicked: " + $(this).text());
+    });
 });
